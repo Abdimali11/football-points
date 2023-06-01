c09f3c4f524af7515ac1349f4f747934b8f388b2
Abdimalik11 <ilkacaze5522@gmail.com>
2023-05-31 17:29:39 -0700

    this is the Answer of football points

--- challenge.js
+++ challenge.js
@@ -1,2 +1,19 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function calculatePoints(wins, draws, losses) {
+    let winPoints = 3;
+   let drawPoints = 1;
+    let lossPoints = 0;
+  
+    let totalPoints = (wins * winPoints) + (draws * drawPoints) + (losses * lossPoints);
+    
+    return totalPoints;
+  }
+  
+  
+  let teamWins = 5;
+  let teamDraws = 3;
+  let teamLosses = 2;
+  
+  let teamPoints = calculatePoints(teamWins, teamDraws, teamLosses);
+  console.log("Total points: " + teamPoints);
